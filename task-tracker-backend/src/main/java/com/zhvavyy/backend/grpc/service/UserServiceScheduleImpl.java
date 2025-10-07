1f6af3ab11e770e7eba2e913ac225a7aa2ac30dc
Zhvavyi Oleg <argumentoleg@gmail.com>
2025-10-07 22:07:25 +0500

    create gRPC service

---
+++ task-tracker-backend/src/main/java/com/zhvavyy/backend/grpc/service/UserServiceScheduleImpl.java
@@ -0,0 +1,36 @@
+package com.zhvavyy.backend.grpc.service;
+
+
+import com.my.grpc.user.UserService;
+import com.my.grpc.user.UserServiceScheduleGrpc;
+import com.zhvavyy.backend.dto.UserReadDto;
+import com.zhvavyy.backend.service.UserServiceImpl;
+import io.grpc.stub.StreamObserver;
+
+import java.util.List;
+
+public class UserServiceScheduleImpl extends UserServiceScheduleGrpc.UserServiceScheduleImplBase {
+
+    UserServiceImpl userService;
+    @Override
+    public void getAll(UserService.SchedulerUsersRequest request, StreamObserver<UserService.UsersResponse> responseObserver) {
+        List<UserReadDto> users = userService.getAll();
+        UserService.UsersResponse.Builder response = UserService.UsersResponse
+                .newBuilder();
+
+        for(UserReadDto user: users){
+            UserService.UserDto userDto =
+                    UserService.UserDto.newBuilder()
+                            .setId(user.id())
+                            .setEmail(user.email())
+                            .setRole(user.role().getAuthority())
+                            .setFirstname(user.firstname())
+                            .setLastname(user.lastname())
+                            .build();
+            response.addUsers(userDto);
+        }
+
+        responseObserver.onNext(response.build());
+        responseObserver.onCompleted();
+    }
+}
