d6b55cd002d81622283d167f85f2d113abf8b900
Zhvavyi Oleg <argumentoleg@gmail.com>
2025-10-07 14:16:39 +0500

    change app.yaml/change service

--- task-tracker-scheduler/src/main/java/com/zhvavyy/scheduler/service/SchedulerServiceImpl.java
+++ task-tracker-scheduler/src/main/java/com/zhvavyy/scheduler/service/SchedulerServiceImpl.java
@@ -1,8 +1,14 @@
 package com.zhvavyy.scheduler.service;
 
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 @Service
-public class SchedulerServiceImpl {
+public class SchedulerServiceImpl implements SchedulerService {
 
+    @Scheduled(cron = "${task.cron.expression}" )
+    @Override
+    public void scheduleUserReport() {
+
+    }
 }
