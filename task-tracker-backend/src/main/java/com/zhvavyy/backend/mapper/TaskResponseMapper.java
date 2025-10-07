f7a6fdd726f4cc10a6654988c9e717604788cd46
Zhvavyi Oleg <argumentoleg@gmail.com>
2025-10-07 16:48:45 +0500

    add new mapper

--- task-tracker-backend/src/main/java/com/zhvavyy/backend/mapper/TaskResponseMapper.java
+++ task-tracker-backend/src/main/java/com/zhvavyy/backend/mapper/TaskResponseMapper.java
@@ -1,4 +1,18 @@
 package com.zhvavyy.backend.mapper;
 
-public class TaskResponseMapper {
+import com.zhvavyy.backend.dto.TaskResponse;
+import com.zhvavyy.backend.model.Task;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TaskResponseMapper implements Mapper<Task, TaskResponse> {
+
+    @Override
+    public TaskResponse mapTo(Task object) {
+        return new TaskResponse(
+                object.getId(),
+                object.getTitle(),
+                object.getStatus()
+        );
+    }
 }
