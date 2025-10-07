4c9ba91b00ecf558428204429768a2145a0b1a53
Zhvavyi Oleg <argumentoleg@gmail.com>
2025-10-07 10:16:29 +0500

    minor edits

--- task-tracker-backend/src/main/java/com/zhvavyy/backend/kafka/messaging/producer/RegisterProducer.java
+++ task-tracker-backend/src/main/java/com/zhvavyy/backend/kafka/messaging/producer/RegisterProducer.java
@@ -2,6 +2,7 @@ package com.zhvavyy.backend.kafka.messaging.producer;
 
 import com.zhvavyy.backend.kafka.messaging.dto.DataForSendEmail;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
@@ -9,9 +10,11 @@ import org.springframework.stereotype.Component;
 @Component
 public class RegisterProducer {
 
+    @Value("${app.kafka.topics.user-registration}")
+    private String userRegister;
     private final KafkaTemplate<String,DataForSendEmail> kafkaTemplate;
 
     public void sendMessage(DataForSendEmail data){
-kafkaTemplate.send("EMAIL_SENDING_TASKS",data);
+        kafkaTemplate.send(userRegister,data);
     }
 }
