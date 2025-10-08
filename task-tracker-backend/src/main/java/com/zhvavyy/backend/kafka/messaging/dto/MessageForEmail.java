7b35ca1b463b789d6ffe73e2f9207891de771cab
Zhvavyi Oleg <argumentoleg@gmail.com>
2025-10-08 11:36:01 +0500

    create new dto

---
+++ task-tracker-backend/src/main/java/com/zhvavyy/backend/kafka/messaging/dto/MessageForEmail.java
@@ -0,0 +1,16 @@
+package com.zhvavyy.backend.kafka.messaging.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class MessageForEmail {
+    private String recipient;
+    private String msgBody;
+    private String subject;
+}
