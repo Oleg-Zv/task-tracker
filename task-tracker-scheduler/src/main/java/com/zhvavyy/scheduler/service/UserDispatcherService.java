0e62f92895b7e89c62d38577d0ff98105308a8bf
Zhvavyi Oleg <argumentoleg@gmail.com>
2025-10-08 22:10:55 +0500

    method report

--- task-tracker-scheduler/src/main/java/com/zhvavyy/scheduler/service/UserDispatcherService.java
+++ task-tracker-scheduler/src/main/java/com/zhvavyy/scheduler/service/UserDispatcherService.java
@@ -1,4 +1,23 @@
 package com.zhvavyy.scheduler.service;
 
-public class UserDispetcher {
+import com.zhvavyy.scheduler.kafka.messaging.dto.MessageForEmail;
+import com.zhvavyy.scheduler.kafka.messaging.producer.UserReportProducer;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class UserDispatcherService {
+
+    private final UserReportService userReportService;
+    private final UserReportProducer producer;
+
+    public void dispatcherUserReport(){
+        List<MessageForEmail> messageList = userReportService.formingDto();
+        for(MessageForEmail dto : messageList){
+            producer.sendMessage(dto);
+        }
+    }
 }
