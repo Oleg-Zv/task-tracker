ddab6924666e3f92785cf55ec4742dba37dc2460
Zhvavyi Oleg <argumentoleg@gmail.com>
2025-10-08 14:49:10 +0500

    delete methods/refactoring

--- task-tracker-backend/src/main/java/com/zhvavyy/backend/controller/TaskController.java
+++ task-tracker-backend/src/main/java/com/zhvavyy/backend/controller/TaskController.java
@@ -1,8 +1,11 @@
 package com.zhvavyy.backend.controller;
 
 
+import com.zhvavyy.backend.dto.TaskCreateDto;
 import com.zhvavyy.backend.dto.TaskReadDto;
-import com.zhvavyy.backend.service.TaskServiceImpl;
+import com.zhvavyy.backend.model.enums.Status;
+import com.zhvavyy.backend.service.TaskService;
+import jakarta.validation.Valid;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
@@ -11,23 +14,30 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @Slf4j
 @Validated
-@RequestMapping("/app/v1/")
+@RequestMapping("/app/v1/tasks")
 @RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class TaskController {
 
-    TaskServiceImpl taskService;
+    TaskService taskService;
 
-    @GetMapping("/tasks")
-    public ResponseEntity<Page<TaskReadDto>> getAllTasks(Pageable pageable){
-        Page<TaskReadDto> all = taskService.getAll(pageable);
-        return ResponseEntity.ok(all);
+    @GetMapping
+    public ResponseEntity<Page<TaskReadDto>> getTasks(@RequestParam(required = false) Status status,
+                                                              Pageable pageable){
+        if(status!=null) {
+            return ResponseEntity.ok(taskService.findAllByStatus(status, pageable));
+        }
+        return ResponseEntity.ok(taskService.getAll(pageable));
+    }
+
+    @PostMapping
+    public ResponseEntity<TaskReadDto> add(@RequestBody @Valid TaskCreateDto taskCreateDto) {
+        TaskReadDto task = taskService.add(taskCreateDto);
+        return ResponseEntity.ok(task);
     }
 }
