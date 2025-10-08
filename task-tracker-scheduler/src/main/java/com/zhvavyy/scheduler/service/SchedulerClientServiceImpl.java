7049c302f74427301c0b9332e8ee96a92911c01d
Zhvavyi Oleg <argumentoleg@gmail.com>
2025-10-08 11:45:28 +0500

    new service client(grpc)

---
+++ task-tracker-scheduler/src/main/java/com/zhvavyy/scheduler/service/SchedulerClientServiceImpl.java
@@ -0,0 +1,27 @@
+package com.zhvavyy.scheduler.service;
+
+import com.my.grpc.user.UserService;
+import com.my.grpc.user.UserServiceScheduleGrpc;
+import net.devh.boot.grpc.client.inject.GrpcClient;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SchedulerClientServiceImpl implements SchedulerService {
+
+@GrpcClient("user-service")
+   private UserServiceScheduleGrpc.UserServiceScheduleBlockingStub stub;
+
+
+    @Scheduled(cron = "${task.cron.expression}" )
+    @Override
+    public void scheduleUserReport() {
+        UserService.SchedulerUsersRequest request= UserService.SchedulerUsersRequest
+                .newBuilder().build();
+
+       UserService.UsersResponse response =  stub.getAll(request);
+
+        System.out.println(response);
+
+    }
+}
