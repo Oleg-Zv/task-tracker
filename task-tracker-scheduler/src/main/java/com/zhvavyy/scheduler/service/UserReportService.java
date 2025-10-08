394ae46e6ec3803851dbebaad9334966373f3381
Zhvavyi Oleg <argumentoleg@gmail.com>
2025-10-08 22:09:01 +0500

    add 3 methods

--- task-tracker-scheduler/src/main/java/com/zhvavyy/scheduler/service/UserReportService.java
+++ task-tracker-scheduler/src/main/java/com/zhvavyy/scheduler/service/UserReportService.java
@@ -1,4 +1,33 @@
 package com.zhvavyy.scheduler.service;
 
+import com.my.grpc.user.UserService;
+
+import com.zhvavyy.scheduler.kafka.messaging.dto.MessageForEmail;
+import com.zhvavyy.scheduler.kafka.messaging.mapper.UserMessageMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class UserReportService {
+
+    private final GrpcUserClientService grpcClientService;
+    private final UserMessageMapper listDto;
+    private final GrpcTaskClientService taskClientService;
+
+    public List<UserService.UserDto> formingUsersList() {
+        return grpcClientService.getResponse().getUsersList();
+    }
+
+    public List<MessageForEmail> formingDto(){
+        List<UserService.UserDto> users=formingUsersList();
+        return listDto.mapTo(users);
+    }
+
+    public void userId(){
+        for(UserService.UserDto user: formingUsersList()){
+            taskClientService.getResponseTask(user.getId());
+        }
+    }
 }
