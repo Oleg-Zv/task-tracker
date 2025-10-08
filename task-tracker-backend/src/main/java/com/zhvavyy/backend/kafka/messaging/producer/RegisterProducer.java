5c54a6343888f2c930dd90a3828b0df9bb3d59c5
Zhvavyi Oleg <argumentoleg@gmail.com>
2025-10-08 11:55:25 +0500

    change args method

--- task-tracker-backend/src/main/java/com/zhvavyy/backend/kafka/messaging/producer/RegisterProducer.java
+++ task-tracker-backend/src/main/java/com/zhvavyy/backend/kafka/messaging/producer/RegisterProducer.java
@@ -1,6 +1,6 @@
 package com.zhvavyy.backend.kafka.messaging.producer;
 
-import com.zhvavyy.backend.kafka.messaging.dto.DataForSendEmail;
+import com.zhvavyy.backend.kafka.messaging.dto.MessageForEmail;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
@@ -12,9 +12,9 @@ public class RegisterProducer {
 
     @Value("${app.kafka.topics.user-registration}")
     private String userRegister;
-    private final KafkaTemplate<String,DataForSendEmail> kafkaTemplate;
+    private final KafkaTemplate<String, MessageForEmail> kafkaTemplate;
 
-    public void sendMessage(DataForSendEmail data){
+    public void sendMessage(MessageForEmail data){
         kafkaTemplate.send(userRegister,data);
     }
 }
