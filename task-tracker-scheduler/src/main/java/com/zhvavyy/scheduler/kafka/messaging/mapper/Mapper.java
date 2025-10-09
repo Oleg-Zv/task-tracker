26e415ae606f5c843188b641b0c378064f58303f
Zhvavyi Oleg <argumentoleg@gmail.com>
2025-10-09 13:32:07 +0500

    delete mapper

--- task-tracker-scheduler/src/main/java/com/zhvavyy/scheduler/kafka/messaging/mapper/Mapper.java
+++
@@ -1,5 +0,0 @@
-package com.zhvavyy.scheduler.kafka.messaging.mapper;
-
-public interface Mapper<F, T> {
-    T mapTo(F object);
-}
--- task-tracker-scheduler/src/main/java/com/zhvavyy/scheduler/service/UserReportService.java
+++ task-tracker-scheduler/src/main/java/com/zhvavyy/scheduler/service/UserReportService.java
@@ -2,10 +2,11 @@ package com.zhvavyy.scheduler.service;
 
 import com.my.grpc.user.UserService;
 
-import com.zhvavyy.scheduler.kafka.messaging.dto.MessageForEmail;
-import com.zhvavyy.scheduler.kafka.messaging.mapper.UserMessageMapper;
+import com.zhvavyy.scheduler.kafka.messaging.mapper.TaskMessageMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -13,21 +14,19 @@ import java.util.List;
 public class UserReportService {
 
     private final GrpcUserClientService grpcClientService;
-    private final UserMessageMapper listDto;
-    private final GrpcTaskClientService taskClientService;
+    private final TaskMessageMapper listDto;
 
     public List<UserService.UserDto> formingUsersList() {
         return grpcClientService.getResponse().getUsersList();
     }
 
-    public List<MessageForEmail> formingDto(){
-        List<UserService.UserDto> users=formingUsersList();
-        return listDto.mapTo(users);
+    public List<Long> getUsersId() {
+        List<UserService.UserDto> users = formingUsersList();
+        List<Long> usersId = new ArrayList<>();
+        for (UserService.UserDto user : users) {
+            usersId.add(user.getId());
+        }
+        return usersId;
     }
 
-    public void userId(){
-        for(UserService.UserDto user: formingUsersList()){
-            taskClientService.getResponseTask(user.getId());
-        }
-    }
 }
