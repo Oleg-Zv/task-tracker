564399064066a22f43abbe1a56bf63782405050b
Zhvavyi Oleg <argumentoleg@gmail.com>
2025-10-11 10:43:37 +0500

    add mailjet field

--- task-tracker-email-sender/entrypoint.sh
+++ task-tracker-email-sender/entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 set -e
 
-export SPRING_MAIL_PASSWORD=$(cat /run/secrets/gmail_password)
+export SPRING_MAILJET_NAME=$(cat /run/secrets/api_key)
+export SPRING_MAILJET_PASSWORD=$(cat /run/secrets/secret_key)
 
 exec java -jar /app/app.jar
