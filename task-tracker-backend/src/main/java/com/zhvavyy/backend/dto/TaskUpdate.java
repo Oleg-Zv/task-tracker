472142b4c3506df1d0f5a64d7ab67e6cff1c2929
Zhvavyi Oleg <argumentoleg@gmail.com>
2025-10-15 17:00:21 +0500

    add: valid for field

--- task-tracker-backend/src/main/java/com/zhvavyy/backend/dto/TaskUpdate.java
+++ task-tracker-backend/src/main/java/com/zhvavyy/backend/dto/TaskUpdate.java
@@ -1,4 +1,12 @@
 package com.zhvavyy.backend.dto;
 
-public class TaskUpdate {
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record TaskUpdate(
+       @NotBlank(message = "Warn: title isEmpty!")
+       @Size(max = 30)
+        String title,
+        @Size(max = 100)
+        String description) {
 }
