8c057c6ce9d56a757a98864dc4f21fb1bae35074
Zhvavyy Oleg <argumentoleg@gmail.com>
2025-10-17 20:09:22 +0500

    change: kill Optional(update method)

--- task-tracker-backend/src/main/java/com/zhvavyy/backend/service/UserService.java
+++ task-tracker-backend/src/main/java/com/zhvavyy/backend/service/UserService.java
@@ -1,10 +1,9 @@
 package com.zhvavyy.backend.service;
 
 import com.zhvavyy.backend.dto.CurrentUserDto;
-import com.zhvavyy.backend.dto.UserCreateDto;
+import com.zhvavyy.backend.dto.UserUpdateDto;
 import com.zhvavyy.backend.dto.UserFilterDto;
 import com.zhvavyy.backend.dto.UserReadDto;
-import com.zhvavyy.backend.model.Task;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -16,7 +15,7 @@ import java.util.Optional;
 public interface UserService {
     CurrentUserDto getUser(UserDetails userDetails);
     Page<UserReadDto> findAllByRole(UserFilterDto filterDto, Pageable pageable);
-    Optional<UserReadDto> update(Long id, UserCreateDto createDto);
+    UserReadDto update(Long id, UserUpdateDto createDto);
     void delete(Long id);
     List<UserReadDto> getAll();
 
