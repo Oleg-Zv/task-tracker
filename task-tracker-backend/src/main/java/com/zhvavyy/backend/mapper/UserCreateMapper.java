8526332f6945b68c49ac7dcf1a8363d4b55e04a5
Zhvavyy Oleg <argumentoleg@gmail.com>
2025-10-17 20:07:37 +0500

    minor edits

--- task-tracker-backend/src/main/java/com/zhvavyy/backend/mapper/UserCreateMapper.java
+++ task-tracker-backend/src/main/java/com/zhvavyy/backend/mapper/UserCreateMapper.java
@@ -1,6 +1,6 @@
 package com.zhvavyy.backend.mapper;
 
-import com.zhvavyy.backend.dto.UserCreateDto;
+import com.zhvavyy.backend.dto.UserUpdateDto;
 import com.zhvavyy.backend.model.User;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
@@ -14,26 +14,26 @@ import java.util.Optional;
 @Component
 @RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
-public class UserCreateMapper implements Mapper<UserCreateDto, User> {
+public class UserCreateMapper implements Mapper<UserUpdateDto, User> {
 
     PasswordEncoder passwordEncoder;
 
 
     @Override
-    public User mapTo(UserCreateDto object) {
+    public User mapTo(UserUpdateDto object) {
         User user = new User();
         copy(object, user);
         return user;
     }
 
     @Override
-    public User mapTo(UserCreateDto fromObject, User toObject) {
+    public User mapTo(UserUpdateDto fromObject, User toObject) {
         copy(fromObject, toObject);
         return toObject;
     }
 
     @Override
-    public void copy(UserCreateDto fromObject, User user){
+    public void copy(UserUpdateDto fromObject, User user){
         user.setFirstname(fromObject.getFirstname());
         user.setLastname(fromObject.getLastname());
         user.setEmail(fromObject.getEmail());
