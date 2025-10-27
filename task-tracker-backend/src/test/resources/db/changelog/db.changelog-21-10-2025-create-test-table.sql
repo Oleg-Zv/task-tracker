4e9e869ef61bd17504e0d2b39063f7b5409a859b
Zhvavyy Oleg <argumentoleg@gmail.com>
2025-10-27 16:48:49 +0500

    add: changelog(create tables)

---
+++ task-tracker-backend/src/test/resources/db/changelog/db.changelog-21-10-2025-create-test-table.sql
@@ -0,0 +1,22 @@
+--liquibase formate
+
+--changeset zhv:create-test-table-users
+CREATE TABLE IF NOT EXISTS users(
+                                    id BIGSERIAL PRIMARY KEY,
+                                    email VARCHAR(64) NOT NULL UNIQUE,
+    firstname VARCHAR(32) NOT NULL,
+    lastname VARCHAR(64) NOT NULL,
+    password VARCHAR(255)NOT NULL,
+    role VARCHAR(32) NOT NULL
+    );
+
+--changeset zhv:create-test-table-tasks
+CREATE TABLE IF NOT EXISTS tasks(
+                                    id BIGSERIAL PRIMARY KEY,
+                                    title VARCHAR(32) NOT NULL,
+    description TEXT NOT NULL,
+    status VARCHAR(32) NOT NULL,
+    user_id BIGINT NOT NULL REFERENCES users(id)ON DELETE CASCADE,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
+    done_at TIMESTAMP WITH TIME ZONE
+                                                    );
